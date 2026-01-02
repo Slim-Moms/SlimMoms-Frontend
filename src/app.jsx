0071276a0557e3c9e11023076677f1a3e4fff871
omertaske <omertaskesenn@gmail.com>
2026-01-02 15:40:58 +0300

    feat: add Loader component with spinner and styles

---
+++ src/app.jsx
@@ -0,0 +1,4 @@
+import { Loader } from "./components/Loader/Loader.jsx";
+
+
+<Loader/>
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,19 @@
+import { RingLoader } from 'react-spinners';
+import css from './LoaderNew.module.css';
+
+export const LoaderNew = () => {
+  return (
+
+
+
+
+    <div className={css.container}>
+        
+      <RingLoader color="#FFA500" size={100} />
+      <div>
+        
+      </div>
+    </div>
+
+  );
+};
